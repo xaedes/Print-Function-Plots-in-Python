ab5d11d10f0c15878108b9fa6bef39d8a9721dad
xaedes <xaedes@gmail.com>
2014-02-17 04:51:07 +0100

    cleaned up code

--- plot_fun.py
+++ plot_fun.py
@@ -3,46 +3,21 @@
 import numpy as np
 import math
 
-
-
-# sparks = ['▁','▂','▃','▄','▅','▆','▇','█']
-
-
-# def getSparks( numbers ):
-#     maximum = max(numbers)
-#     minimum = min(numbers)
-
-#     div = float(maximum - minimum)
-#     if div == 0:
-#         div = 1
-#     return ''.join( sparks[int((n - minimum)/div * 7)] for n in numbers)
-
-
-# def stringToNumberList( string ):
-#     numbers = []
-#     for n in string:
-#         try:
-#             numbers.append(float(n))
-#         except ValueError:
-#             pass
-#     return numbers
-
 def plot_fun(func,begin = 0.,end = 1., width=20, height=10, only_return = False):
     x_chr = np.arange(width)
     y_chr = np.arange(height)
     x = begin + (end - begin) * np.hstack([x_chr,[width]]) / float(width)
     y = np.array(map(func,x))
-    # print getSparks(list(y))
-    # print "foo"
 
     y_min = np.min(y)
     y_max = np.max(y)
 
+    # initialize char grid
     grid=[]
     for i in range(height):
         grid.append([' ']*width)
-    # print y_min
-    # print y_max
+
+    # plot function into char grid
     for i in range(width):
         y_norm = (y[i]-y_min) / (y_max-y_min)
         y_chr_idx = int(math.floor((1-y_norm) * (height-1)))
@@ -59,9 +34,9 @@ def plot_fun(func,begin = 0.,end = 1., width=20, height=10, only_return = False)
             char = '\\'
         grid[y_chr_idx][i] = char
 
-    # xs, ys = meshgrid(x_chr, y)
-    # bounds = map(lambda grid_line: map(lambda grid_cell,grid_line),grid)
+    ### string auxiliary functions
 
+    # align two texts in a given length of string
     def align_left_and_right(left,right,width,padding=1):
         if width-len(left)-len(right)+padding > 0:
             return left+' ' * (width-len(left)-len(right)+padding) + right
@@ -72,26 +47,25 @@ def plot_fun(func,begin = 0.,end = 1., width=20, height=10, only_return = False)
             width_for_right = width_minus_padding - width_for_left
             return left[:width_for_left] + ' ' * padding + right[:width_for_right]
 
+    # string representation of a point given by tuple
     def str_point(point):
         return ';'.join(map(str,list(point)))
 
+
+    # build resulting string
     lines = []
     lines.append(align_left_and_right(str_point((begin,y_max)),str_point((end,y_max)),width))
     lines.extend( map(lambda arr: "".join(arr),grid) )
     lines.append(align_left_and_right(str_point((begin,y_min)),str_point((end,y_min)),width))
 
+    # convert to string
     string = "\n".join(lines)
 
+    # print 
     if not only_return:
         print string
 
     return string
 
 if __name__ == '__main__':
-    fun = lambda dt_now, self_dt: pow(1./2000.,abs(dt_now - self_dt))
-    fun2 = lambda x: fun(dt_now=x+0.0,self_dt=0.0)
-
-
     plot_fun(lambda x: math.sin(x),begin=0,end=1*1*math.pi,width=80,height=10)
-    # plot_fun(math.sin,end=0.5,width=80,height=30)
-    # plot_fun(fun2,end=0.5,width=40,height=10)
